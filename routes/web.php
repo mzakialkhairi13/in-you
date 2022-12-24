f0f5a232c672f035b8b77fd63e967ae3036e5862
mzakialkhairi13 <muhammadzakialkhairi@gmail.com>
2022-12-24 23:48:54 +0700

    logout

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/register',[SigninController::class, 'register']);
 Route::get('/login',[SigninController::class, 'login']);
 Route::post('/register',[SigninController::class, 'register_store']);
 Route::post('/login',[SigninController::class, 'authenticate']);
+Route::get('/logout',[SigninController::class, 'logout']);
 
 Route::get('/admin', [AdminController::class, 'index']);
 Route::get('/admin/paket', [AdminController::class, 'index']);
